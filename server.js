7b537d12a684f1a038c13ac4beb3a3bd11d4bcef
davidcastel <davidccbp20@gmail.com>
2023-04-07 13:10:23 -0400

    implement routes from dif module

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 "use strict";
 
+const Routes = require("./lib/routes");
 const Hapi = require("@hapi/hapi");
 const Settings = require("./settings");
 
 const init = async () => {
   const server = new Hapi.Server({ port: Settings.port });
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "Hello, world!";
-    }
-  });
+  server.route(Routes);
 
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);

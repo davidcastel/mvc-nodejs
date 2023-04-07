ec974c47790a77f2cb967bc0dc72a54bf90f21b2
davidcastel <davidccbp20@gmail.com>
2023-04-07 10:43:04 -0400

    set settings for db

---
+++ settings.js
@@ -0,0 +1,22 @@
+// This will load our .ENV file andd add the values to process.env
+
+require("dotenv").config({ silent: true });
+
+module.export = {
+    port: process.env.PORT || 3000,
+    env: process.env.NODE_ENV || "development",
+
+    // Environment-Dependent Settings
+    development: {
+        db: {
+            dialect: "sqlite3",
+            storage: ":memory:"
+        }
+    },
+    production: {
+        db: {
+            dialect: "sqlite3",
+            storage: "db/database.sqlite3"
+        }
+    }
+};

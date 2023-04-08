a46d8871a1cd9b1d3eaf7e2f1b5551c97ba302c0
davidcastel <davidccbp20@gmail.com>
2023-04-07 21:49:24 -0400

    synchronize the models to our database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Import the index.js file inside the models dir
+const Models = require("./lib/models/");
 const Routes = require("./lib/routes");
 const Hapi = require("@hapi/hapi");
 const Settings = require("./settings");
@@ -8,6 +10,8 @@ const init = async () => {
   const server = new Hapi.Server({ port: Settings.port });
 
   server.route(Routes);
+
+  await Models.sequelize.sync();
 
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);

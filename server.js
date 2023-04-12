893e305bbec9aa23381b25514de6dd60cda25b59
davidcastel <davidccbp20@gmail.com>
2023-04-12 18:14:15 -0400

    Add settings to our views

--- server.js
+++ server.js
@@ -5,10 +5,23 @@ const Models = require("./lib/models/");
 const Routes = require("./lib/routes");
 const Hapi = require("@hapi/hapi");
 const Settings = require("./settings");
+const Path = require("path");
 
 const init = async () => {
   const server = new Hapi.Server({ port: Settings.port });
+  
+  await server.register([require("@hapi/vision")]);
 
+  server.views({
+    engines: { pug: require("pug") },
+    path: Path.join(__dirname, "lib/views"),
+    compileOptions: {
+      pretty:false
+    },
+    isCached: Settings.env === "production"
+  });
+
+  // Add routes
   server.route(Routes);
 
   await Models.sequelize.sync();
